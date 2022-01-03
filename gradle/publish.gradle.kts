8b3356885142a8b0bb1b02c0a8dbb56917e44c2b
arthur.chen <efsn.chan@gmail.com>
2022-01-04 00:28:31 +0800

    feat: add gradle publish script

---
+++ gradle/publish.gradle.kts
@@ -0,0 +1,37 @@
+plugins {
+    `maven-publish` apply false
+}
+subprojects {
+    apply(plugin = "maven-publish")
+    configure<PublishingExtension> {
+        repositories {
+            maven {
+                name = "GitHubPackages"
+                url = uri("https://maven.pkg.github.com/OWNER/REPOSITORY")
+                credentials {
+                    username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
+                    password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
+                }
+            }
+        }
+        publications {
+            register<MavenPublication>("gpr") {
+                from(components["java"])
+            }
+        }
+    }
+}
+
+
+// for build dependencies
+repositories {
+    maven {
+        url = uri("https://maven.pkg.github.com/OWNER/REPOSITORY")
+        credentials {
+            username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
+            password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
+        }
+    }
+}
+
+
