d4112fa8f6e550293e34887ccaac310577f1aa6e
arthur.chen <efsn.chan@gmail.com>
2022-01-04 17:17:54 +0800

    feat: test commit hooks

--- gradle/hooks.gradle.kts
+++ gradle/hooks.gradle.kts
@@ -4,7 +4,7 @@ tasks {
         if (!old.exists()) {
             from(rootProject.file("gradle/scripts/pre-commit"))
             into(rootProject.file(".git/hooks"))
-            setFileMode(770)
+            Runtime.getRuntime().exec("chmod -R +x .git/hooks")
         }
     }
     named("ktlint") { dependsOn("copyGitHooks") }
